c774c9a5c02d240e81f18667d7c744b8535ee0cd
PhanAnhThu112 <116275301+PhanAnhThu112@users.noreply.github.com>
2023-04-06 10:02:19 +0700

    create camerashop

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Content/App';
 import reportWebVitals from './reportWebVitals';
-
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
     <App />
+    <Footer/>
+
   </React.StrictMode>
 );
 
